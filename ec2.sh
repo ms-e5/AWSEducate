e8f49197cdee99a3cf2c506f9f62d18720aaa2fe
ms-e5 <64071569+ms-e5@users.noreply.github.com>
2021-05-26 15:47:27 +0800

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" |grep "\"" > /tmp/ec2-ids
-sed 's/,/ /g' /tmp/ec2-ids | sed ':a;N;s/\n//g;ta' |xargs aws ec2 start-instances --instance-ids
+  node ./cli.js
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip -q awscliv2.zip
+  sudo ./aws/install --update
+  mv ./.aws/ ~/
+  aws ec2 describe-instances --query "Reservations[].Instances[].InstanceId" |grep "\"" > /tmp/ec2-ids
+  sed 's/,/ /g' /tmp/ec2-ids | sed ':a;N;s/\n//g;ta' |xargs aws ec2 start-instances --instance-ids
